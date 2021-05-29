77e8621bb29d28b7c42d936f4f308db2205dff91
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2021-05-29 14:32:37 +1000

    Update and rename gauth.py to googleauth.py

--- googleauth.py
+++ googleauth.py
@@ -4,21 +4,17 @@ system_random = random.SystemRandom()
 class Object(object):
   pass
 
-class one():
-  def hello():
-    return "hi"
-
-def gen_token(length, period=5):
+def generate(length, period=5):
   token = Object()
   period = period + 1
   if period < 2:
     raise Exception("period cannot be 0 or negitive.")
-  token.defualt = str()
+  token.token = str()
   token.period = str()
   repeat = 0
   for loop in range(0, length):
       repeat = repeat + 1
-      character = system_random.randint(1, 32)
+      character = system_random.randint(2, 32)
       if not character < 7:
         string = "qwertyuiopasdfghjklzxcvbnm"
         character = str(string[character - 7])
@@ -28,16 +24,17 @@ def gen_token(length, period=5):
         repeat = 0
       else:
         token.period = f"{token.period}{character}"
-      token.defualt = f"{token.defualt}{character}"
+      token.token = f"{token.token}{character}"
   return token
 
-def hotp(secret, intervals_no):
-  key = base64.b32decode(secret, True)
-  msg = struct.pack(">Q", intervals_no)
-  h = hmac.new(key, msg, hashlib.sha1).digest()
-  o = h[19] & 15
-  h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
-  return h
-
-def totp(secret):
-  return hotp(secret, intervals_no=int(time.time())//30)
+class get():
+  def hotp(secret, intervals_no):
+    key = base64.b32decode(secret, True)
+    msg = struct.pack(">Q", intervals_no)
+    h = hmac.new(key, msg, hashlib.sha1).digest()
+    o = h[19] & 15
+    h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
+    return h
+    
+  def totp(secret):
+    return get.hotp(secret, intervals_no=int(time.time())//30)
